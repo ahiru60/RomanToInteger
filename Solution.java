5a4c605dbaa4f98d930fe44b253cd55cd2b05a32
Adithya Karunarathna <64568139+ahiru60@users.noreply.github.com>
2022-12-08 19:45:23 +0530

    Delete Solution.java

--- Solution.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-
-class Solution {
-    String s = "III";
-    public int romanToInt(String s) {
-        List letterI = s.stream().split( "" ).filter(l->l.startsWith("I"));
-        System.out.println(letterI[0]);
-    }
-}
